658613d691925d72c92518c38f9a29fb5f1b7ca8
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2019-08-26 22:53:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TodoList from "./TodoList";
+
+function App() {
+  return (
+    <div className="App">
+<h1>
+See Our Todos :
+</h1>
+<TodoList/>
+
+    </div>
+  );
+}
+
+export default App;
