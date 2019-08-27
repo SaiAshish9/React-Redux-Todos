406e5df708581d5294306b6a3dc6efbe12731717
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2019-08-27 22:38:01 +0530

    Add files via upload

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,75 +1,54 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import Todo from "./Todo";
-import {connect} from "react-redux";
-import {addTodo,removeTodo} from "./actionCreators";
-import {Route} from "react-router"
-import NewTodoForm from "./NewTodoForm"
+import NewTodoForm from "./NewTodoForm";
+import { connect } from "react-redux";
+import { addTodo, removeTodo, getTodos } from "./actionCreators";
+import { Route } from "react-router-dom";
 
- class TodoList extends Component{
-constructor(props){
-  super(props);
-  this.handleAdd=this.handleAdd.bind(this)
-
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+  componentDidMount() {
+    // debugger;
+    this.props.getTodos();
+  }
+  handleAdd(val) {
+    this.props.addTodo(val);
+  }
+  removeTodo(id) {
+    this.props.removeTodo(id);
+  }
+  render() {
+    let todos = this.props.todos.map(val => (
+      <Todo
+        removeTodo={this.removeTodo.bind(this, val._id)}
+        task={val.task}
+        key={val._id}
+      />
+    ));
+    return (
+      <div>
+        <Route
+          path="/todos/new"
+          component={props => (
+            <NewTodoForm {...props} handleSubmit={this.handleAdd} />
+          )}
+        />
+        <Route exact path="/todos" component={() => <div>{todos}</div>} />
+      </div>
+    );
+  }
 }
 
-handleAdd(val){
-  this.props.addTodo(val)
-}
-// handleSubmit(e){
-//   e.preventDefault();
-// this.props.addTodo(this.state.task)
-//
-// e.target.reset()
-// }
-//
-// handleChange(e){
-//   this.setState({
-//   [e.target.name]:[e.target.value]
-// })
-// }
-
-removeTodo(id){
-  this.props.removeTodo(id);
+function mapStateToProps(reduxState) {
+  // debugger;
+  return {
+    todos: reduxState.todos
+  };
 }
 
-render(){
-  let todos=this.props.todos.map((val,index)=>(
-    <Todo removeTodo={this.removeTodo.bind(this,val.id)} task={val.task} key={index}/>
-
-  ));
-  return (
-<div>
-<Route path="/todos/new" component={props=>(
-  <NewTodoForm {...props} handleSubmit={this.handleAdd}/>
-)}/>
-
-<Route exact path="/todos" component={()=><div>
-<ul>
-{todos}
-</ul>
-</div>
-}/>
-</div>
-
+export default connect(mapStateToProps, { addTodo, removeTodo, getTodos })(
+  TodoList
 );
-}
-}
-
-function mapStateToProps(reduxState){
-return {
-todos:reduxState.todos
-}
-}
-
-// function mapDispatchToProps(dispatch){
-//   return
-//   {
-//     addTodo:(task)=>{
-//       type:"ADD_TODO",
-//       task
-//
-//     }
-//   }
-// }
-
-export default connect(mapStateToProps,{addTodo,removeTodo})(TodoList);
